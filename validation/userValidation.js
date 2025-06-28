1124fac431285f97d377aaa7034957ff5052e79b
MBbaha <168421621+MBbaha@users.noreply.github.com>
2025-06-28 15:09:24 +0500

    Update userValidation.js

--- validation/userValidation.js
+++ validation/userValidation.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const userRegisterValidationSchema = Joi.object({
   firstname: Joi.string().trim().min(3).max(30).required(),
   lastname: Joi.string().trim().min(3).max(30).required(),
-  phonenumber: Joi.string().trim().length(13).required(),
+  phonenumber: Joi.string().trim().length(9).required(),
   sana: Joi.string().required(), // yoki .isoDate()
   summa: Joi.string().required(),
   location:Joi.string().required(),
@@ -14,7 +14,7 @@ const userRegisterValidationSchema = Joi.object({
 const userUpdateValidationSchema = Joi.object({
   firstname: Joi.string().trim().min(3).max(30),
   lastname: Joi.string().trim().min(3).max(30),
-  phonenumber: Joi.string().trim().length(13),
+  phonenumber: Joi.string().trim().length(9),
   sana: Joi.string(),
   summa: Joi.string(),
   location:Joi.string(),
